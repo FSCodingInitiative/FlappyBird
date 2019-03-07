3816533abe1d02189a77511ac8bed384f940eeaf
markusbz <Moin5647>
2019-03-07 21:23:39 +0100

    first test pygame

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import sys, pygame
 pygame.init()
 
 screen = pygame.display.set_mode((414, 736))
-black = [255,255,255]
 
 while 1:
-    screen.fill(black)
+    screen.fill(233,23,131)
+    c = 0
+    for i in range(100000):
+        c = i
+    sys.exit()

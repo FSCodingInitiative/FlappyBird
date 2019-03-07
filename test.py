fe75f75af98b0dfa1a0e5a6883450e5e2ba9f9e3
lukas123 <lukas.mertensmeyer@fs-students.de>
2019-03-07 21:16:03 +0100

    first_code

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
 import sys, pygame
 pygame.init()
 
-screen = pygame.display.set_mode((414, 736))
-black = [255,255,255]
+class FlappyBird:
 
-while 1:
-    screen.fill(black)
+    def run(self):
+
+
+        bird =  pygame.image.load("Graphics/Bird.png")
+        bird = pygame.transform.scale(bird, (100, 100))
+
+        #Position bird
+        birdrect = bird.get_rect()
+        birdrect = birdrect.move((170, 310))
+
+        #opens screen
+        screen = pygame.display.set_mode((414, 736))
+        background = 99, 209, 62
+
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+
+                screen.fill(background)
+                screen.blit(bird, birdrect)
+                #updates background
+                pygame.display.flip()
+
+
+if __name__ == "__main__":
+    FlappyBird().run()
+

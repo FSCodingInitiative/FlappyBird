997ba2259b6885b2095aee75b72c639544afcdad
markusbz <Moin5647>
2019-03-14 21:35:20 +0100

    first test pygame

--- test.py
+++ test.py
@@ -11,6 +11,10 @@ class FlappyBird:
         #background RGB
         background = 135, 206, 235
 
+        #define pipe speed
+        pipe_speed = 0.6
+
+
         #Bird
         #Loading and sizing
         bird =  pygame.image.load("Graphics/Bird.png")
@@ -20,16 +24,17 @@ class FlappyBird:
         birdrect = birdrect.move((170, 310))
 
         #pipes
+        init_pos = 200
         pipes = pygame.image.load("Graphics/pipes.png")
         pipes = pygame.transform.scale(pipes, (150, 400))
         pipesdrect = pipes.get_rect()
-        pipesdrect = pipesdrect.move((200, 520))
+        pipesdrect = pipesdrect.move((init_pos, 520))
 
         #Reversed pipes
         #Rotates normal pipes and creates reversed as new variable
         pipes_rev = pygame.transform.rotate(pipes, 180)
         pipesrevrect = pipes_rev.get_rect()
-        pipesrevrect = pipesrevrect.move((200, 0))
+        pipesrevrect = pipesrevrect.move((init_pos, 0))
 
         #opens screen
         screen = pygame.display.set_mode((x, y))
@@ -38,16 +43,31 @@ class FlappyBird:
         while 1:
             #Needed to end pygame
             for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-                #Creates background
+            #Creates background
+            screen.fill(background)
+            #Load objects in screen
+
+            #updates background
+            pygame.display.flip()
+
+            for i in range(10000):
+                #define new position of pipe
+                init_pos -= pipe_speed
+                #delete old pipes on screen
                 screen.fill(background)
-                #Load objects in screen
-                screen.blit(pipes, pipesdrect)
-                screen.blit(pipes_rev, pipesrevrect)
+                #create new moved pipe
+                screen.blit(pipes, pipesdrect.move((init_pos, 0)))
+                screen.blit(pipes_rev, pipesrevrect.move((init_pos, 0)))
                 screen.blit(bird, birdrect)
-                #updates background
-                pygame.display.flip()
+                pygame.display.update()
+
+
+
+                pygame.time.delay(2)
+
 
 #Makes it possible to run code with terminal and without creating new objects
 if __name__ == "__main__":

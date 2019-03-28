3907860db3a39a083b7e84a3d4b0b0bb8a9e6aed
Nicolas Kepper <nicolas.kepper@gmail.com>
2019-03-28 22:16:30 +0100

    PipePairs, dynamic drawing, dynamic creation

--- Pipe.py
+++ Pipe.py
@@ -1,13 +1,38 @@
 import pygame
 
+class PipePair:
+
+    space = 750
+
+    def __init__(self, xpos, ypos):
+        self.bot = Pipe(xpos, ypos, False)
+        self.top = Pipe(xpos, ypos - PipePair.space, True)
+
+    def show(self, screen):
+        self.top.show(screen)
+        self.bot.show(screen)
+
+    def move_x(self, offset):
+        self.top.move_x(offset)
+        self.bot.move_x(offset)
+
 class Pipe:
+
     def __init__(self, xpos, ypos, rev):
         self.xpos = xpos
         self.ypos = ypos
         self.rev = rev
 
-        self.surface = pygame.image.load("Graphics/pipes.png")
+        self.surface = pygame.image.load("Graphics/pipes.png")#.convert()
         self.surface = pygame.transform.scale(self.surface, (197, 400))
         if self.rev:
             self.surface = pygame.transform.rotate(self.surface, 180)
         self.rect = self.surface.get_rect().move((self.xpos, self.ypos))
+
+    def show(self, screen):
+        screen.blit(self.surface, self.rect)
+
+    def move_x(self, offset):
+        self.rect = self.rect.move((offset, 0))
+        self.xpos += offset
+
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sys, pygame
+from Pipe import *
 #Init pygame runs seperately
 pygame.init()
 
@@ -12,56 +13,23 @@ class FlappyBird:
         background = 135, 206, 235
 
         #define pipe speed
-        pipe_speed = 3
+        pipe_speed = 20
 
 
         #Bird
         #Loading and sizing
-        bird =  pygame.image.load("Graphics/Bird.png")
+        bird = pygame.image.load("Graphics/Bird.png")
         bird = pygame.transform.scale(bird, (100, 100))
         #Positioning
         birdrect = bird.get_rect()
         birdrect = birdrect.move((0, 0))
 
-        def create_pipe(xpos, ypos):
-            rev_pipe_ypos = ypos - 500
-            pipes = pygame.image.load("Graphics/pipes.png")
-            pipes = pygame.transform.scale(pipes, (197, 400))
-            pipes_rev = pygame.transform.rotate(pipes, 180)
-            pipes_rect = pipes.get_rect()
-            pipes_rect = pipes_rect.move((xpos, ypos))
-            pipes_rev_rect = pipes_rev.get_rect()
-            pipes_rev_rect = pipes_rev_rect.move((xpos, rev_pipe_ypos))
 
-            return pipes, pipes_rev, pipes_rect, pipes_rev_rect
+        pipes = [PipePair(500, 500)]
 
-        pipes, pipes_rev, pipes_rect, pipes_rev_rect = create_pipe(500, 300)
         #opens screen
         screen = pygame.display.set_mode((x, y))
-
-        """"#create additional pipes
-        pipes_2 = pipes_1.move((197, 0))
-        pipes_r2 = pipes_r1.move((197, y_ax_init_r))
-        init_pos2 = init_pos
-
-        pipes_3 = pipes_1.move((197*2, 0))
-        pipes_r3 = pipes_r1.move((197*2, y_ax_init_r))
-        init_pos3 = init_pos
-
-
-
-
-        #list with positions of every pipe
-        pipe_pos = [init_pos,init_pos2,init_pos3]
-
-        #list of pipes
-        pipe_list = [pipes_1, pipes_2, pipes_3]
-
-        #list of reversed pipes
-        pipe_r_list = [pipes_r1, pipes_r2, pipes_r3]"""
-
-
-
+        clock = pygame.time.Clock()
         while 1:
             #Needed to end pygame
             for event in pygame.event.get():
@@ -72,26 +40,17 @@ class FlappyBird:
             screen.fill(background)
 
             #define new position of pipe
-            """for index, val in enumerate(pipe_pos):
-                if (val -index*197) <= -197*2:
-                    pipe_pos[index] = 1200-197
-                    print("now", index)
-                else:
-                    pipe_pos[index] -= pipe_speed
-                    print("index", index, "val", val)
-            #delete old pipes on screen
-            screen.fill(background)
-            #create new moved pipe
+            for pipePair in pipes:
+                pipePair.move_x(-pipe_speed)
+                if pipePair.top.xpos <= 0:
+                    print(pygame.time.get_ticks())
 
-            for i in range(0,len(pipe_list)):
-                screen.blit(pipes, pipe_list[i].move((pipe_pos[i], y_ax_init)))
-                screen.blit(pipes_rev, pipe_r_list[i].move((pipe_pos[i], y_ax_init_r)))"""
-
-            screen.blit(pipes_rev,pipes_rev_rect)
-            screen.blit(pipes,pipes_rect)
+            for p in pipes:
+                p.show(screen)
 
             screen.blit(bird, birdrect)
-            pygame.display.update()
+            pygame.display.flip()
+            clock.tick(80)
 
 
 

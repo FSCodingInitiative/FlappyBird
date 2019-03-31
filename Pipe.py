20391376c8283b1936351428daf31b14568f8caf
markusbz <Moin5647>
2019-03-31 23:07:32 +0200

    minor changes

--- Pipe.py
+++ Pipe.py
@@ -23,8 +23,7 @@ class Pipe:
         self.ypos = ypos
         self.rev = rev
 
-        self.surface = pygame.image.load("Graphics/pipes.png")
-        self.surface = pygame.transform.scale(self.surface, (197, 400))
+        self.surface = pygame.transform.scale(pygame.image.load("Graphics/pipes.png"), (197, 400))
         if self.rev:
             self.surface = pygame.transform.rotate(self.surface, 180)
         self.rect = self.surface.get_rect().move((self.xpos, self.ypos))
